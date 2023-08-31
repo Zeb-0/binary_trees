5bfbfe0570ea9249d96d5356372644936cca99a4
Zeb-0 <morandezeb@gmail.com>
2023-08-31 22:39:36 +0300

    find uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,9 +2,8 @@
 
 /**
  * binary_tree_uncle - finds the uncle of a node
- * @node: pointer to the node to find the uncle
- *
- * Return: pointer to the uncle node, or NULL if error occurs
+ * @node: points to the node to find the uncle
+ * Return: pointer to the uncle node
  */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
