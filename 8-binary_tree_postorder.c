7dc7a569bfb54470367c04d8937a57685fe682a7
Zeb-0 <morandezeb@gmail.com>
2023-08-31 22:19:59 +0300

    go through a binary tree using post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through binary tree using post-order traversal
+ * binary_tree_postorder - go through binary tree using post-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
- *
  * Return: void
  */
 
